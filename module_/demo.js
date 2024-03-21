3bc09ca830c3bd9f71c5c41c416de107e298cca4
bsnt <tilak.basnet@outlook.com>
2024-03-21 19:07:02 +0100

    Has been added the function and vars

--- module_/demo.js
+++ module_/demo.js
@@ -5,6 +5,6 @@ const dt = require('./my_module');
 http.createServer((req,res)=>{
     res.writeHead(200, {'Content-type': 'text/html'});
     res.write("Current Date and Time is: " + dt.myDateTime());
-    console.log("Current Date
+    console.log("Current Date and Time:" + dt.myDateTime());
     res.end();
 }).listen(8080);
