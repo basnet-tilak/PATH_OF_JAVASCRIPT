debd7f209985b9f37cd50b9f1a3f975dcf65e075
bsnt <tilak.basnet@outlook.com>
2024-03-22 07:14:31 +0100

    Has been added the String method at which find the position of the letter

--- js/string_/String_Quotes.js
+++ js/string_/String_Quotes.js
@@ -1,7 +1,15 @@
 
 let text = "This is simple text with double quotAs";
 let text_bactricks = `This text with backticks`;
+
+var fullName = "Tilak Basnet";
+
 console.log(text);
 console.log(text_bactricks);
 console.log(text.length); // length of the whole text
+
 console.log(text.toLocaleUpperCase());
+
+console.log('Character in fullName at 4 is: '+ fullName.charAt(4)); // k
+console.log('Character in fullName at 4 is: '+fullName.at(4)) // k
+console.log('Character in fullName at 4 is: '+fullName[4]); // k
