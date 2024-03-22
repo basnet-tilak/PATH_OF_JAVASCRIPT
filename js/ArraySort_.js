ffe3d4fa8abda23a9aeba480c8ce46b1312395db
bsnt <tilak.basnet@outlook.com>
2024-03-22 13:49:45 +0100

    Has been added the Sort, min, max, method

--- js/ArraySort_.js
+++ js/ArraySort_.js
@@ -1,5 +1,11 @@
 
 const array_w = ["Tilak","Kshitish","Him","Shubham","Sujan","Devil"];
-const array_x = [11, 2,8,3,2,1];
+const array_x = [7,13,9,8,3,2];
 
-console.log(array_x.sort((a,b) => a - b));
+console.log(array_x.sort((a,b) => a - b));  // sort by ascesdening order
+
+console.log(array_x.sort((a,b) => b - a));  // sort by descending order
+
+console.log(Math.min.apply(null, array_x));  // find the min number from the array
+
+console.log(Math.max.apply(null, array_x));
