f9bbbb565ad561dd943462afe37abe4c8f8467e4
bsnt <tilak.basnet@outlook.com>
2024-03-22 06:55:22 +0100

    Has been added the String

---
+++ js/string_/String_Quotes.js
@@ -0,0 +1,7 @@
+
+let text = "This is simple text with double quotAs";
+let text_bactricks = `This text with backticks`;
+console.log(text);
+console.log(text_bactricks);
+console.log(text.length); // length of the whole text
+console.log(text.toLocaleUpperCase());
