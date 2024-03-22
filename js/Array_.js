93e39efeb7ebb9bc1273de689c1ce96ae600abe0
bsnt <tilak.basnet@outlook.com>
2024-03-22 11:45:53 +0100

    Has been added the Array

---
+++ js/Array_.js
@@ -0,0 +1,16 @@
+
+
+const arrayString_= ["Tilak","Kshitish","Him","Shubham","Sujan","Devil"];
+const array_ = [11,2,6,8,2,21];
+
+console.log(array_[0]);
+
+/*
+for(let i = 0; i<array_.length; i++){
+    console.log(array_[i]);
+}
+*/
+
+array_.forEach(element => {
+    console.log( element)
+});
--- js/string_/String_Quotes.js
+++ js/string_/String_Quotes.js
@@ -10,6 +10,12 @@ console.log(text.length); // length of the whole text
 
 console.log(text.toLocaleUpperCase());
 
-console.log('Character in fullName at 4 is: '+ fullName.charAt(4)); // k
+console.log('Character in fullName at 4 is: '+ fullName.charAt(4)); // k 
 console.log('Character in fullName at 4 is: '+fullName.at(4)) // k
 console.log('Character in fullName at 4 is: '+fullName[4]); // k
+console.log('Index of the k in fullName at: '+fullName.indexOf('k'));  // index of the k is 4
+console.log('FullName slice into first name: '+ fullName.slice(0,5)); // Tilak
+console.log('Character at position end of the fullName is: '+ fullName.at(-1));   // at position end  t
+console.log('Character at position second last of fullName is: '+fullName.at(-2));   // at position second last e
+
+console.log(fullName.trim()) // remove the space begining and end of the string
